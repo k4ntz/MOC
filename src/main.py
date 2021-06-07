cdbe72edc334d696d85e63bf91ac7b1522ac6a33
ThomasRot <thomasalexander.rothenbaecher@stud.tu-darmstadt.de>
2021-06-07 09:04:09 +0200

    MT-3: Initial OpenAiGym data gathering

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from engine.utils import get_config
 from engine.train import train
 from engine.eval import eval
 from engine.show import show
+from dataset.gather import gather
 
 
 if __name__ == '__main__':
@@ -10,6 +11,7 @@ if __name__ == '__main__':
         'train': train,
         'eval': eval,
         'show': show,
+        'gather': gather
     }
     cfg, task = get_config()
     assert task in task_dict
