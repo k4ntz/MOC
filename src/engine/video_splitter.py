684c91252ade3e1fc127c731d00e0808caa31b9e
ThomasRot <thomasalexander.rothenbaecher@stud.tu-darmstadt.de>
2021-06-09 13:25:07 +0200

    MT-3: Initial OpenAiGym data gathering

--- src/engine/video_splitter.py
+++ src/engine/video_splitter.py
@@ -16,7 +16,12 @@ def split_videos(cfg):
         vid = skv.vread(f'videos/{args_env_id}/{args_env_id}_ep{i:06}.mp4')
         for j in range(0, vid.shape[0] - SEQUENCE_LENGTH, 1):
             sub_vid = vid[j:j + SEQUENCE_LENGTH]
-            skv.vwrite(f'data/ATARI/{args_env_id}/train/{args_env_id}_ep{i:06}_seq{j:03}.mp4', sub_vid)
+            if j % 3 == 0:
+                skv.vwrite(f'data/ATARI/{args_env_id}/test/{args_env_id}_ep{i:06}_seq{j:03}.mp4', sub_vid)
+            elif j % 4 == 0:
+                skv.vwrite(f'data/ATARI/{args_env_id}/validation/{args_env_id}_ep{i:06}_seq{j:03}.mp4', sub_vid)
+            else:
+                skv.vwrite(f'data/ATARI/{args_env_id}/train/{args_env_id}_ep{i:06}_seq{j:03}.mp4', sub_vid)
         rtpt.step(subtitle=f"step={i}/{total_files}")
 
 
