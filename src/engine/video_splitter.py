157862af244acb2ffc34face8e6067050a7f2141
ThomasRot <thomasalexander.rothenbaecher@stud.tu-darmstadt.de>
2021-06-09 13:15:52 +0200

    MT-3: Initial OpenAiGym data gathering

--- src/engine/video_splitter.py
+++ src/engine/video_splitter.py
@@ -13,7 +13,7 @@ def split_videos(cfg):
     args_env_id = 'Pong-v0'
 
     for i in range(total_files):
-        vid = skv.vread(f'videos/{args_env_id}/{args_env_id}_ep{episode_id:06}')
+        vid = skv.vread(f'videos/{args_env_id}/{args_env_id}_ep{i:06}')
         for j in range(0, vid.shape[0] - SEQUENCE_LENGTH, 1):
             sub_vid = vid[j:j + SEQUENCE_LENGTH]
             skv.vwrite(f'data/ATARI/{args_env_id}/train/{args_env_id}_ep{i:06}', sub_vid)
