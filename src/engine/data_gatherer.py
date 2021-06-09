4afe23749d8135547a99520c7dde268eebb648d0
ThomasRot <thomasalexander.rothenbaecher@stud.tu-darmstadt.de>
2021-06-09 11:21:56 +0200

    MT-3: Initial OpenAiGym data gathering

--- src/engine/data_gatherer.py
+++ src/engine/data_gatherer.py
@@ -4,12 +4,28 @@ import gym
 from gym import wrappers, logger
 from rtpt import RTPT
 import matplotlib.pyplot as plt
+from gym.wrappers.monitoring.video_recorder import VideoRecorder
+from multiprocessing import Process
 
 # Create RTPT object
 rtpt = RTPT(name_initials='QD', experiment_name='TestingRTPT', max_iterations=10)
 
 # Start the RTPT tracking
 rtpt.start()
+
+
+class GymRenderer:
+    def __init__(self, env, title="video"):
+        self.env = env
+        self.record = record
+        self.video_rec = VideoRecorder(env, path=f"videos/{title}.mp4")
+
+    def render(self):
+        self.video_rec.capture_frame()
+
+    def close_recorder(self):
+        self.video_rec.close()
+        self.video_rec.enabled = False
 
 
 class RandomAgent(object):
@@ -22,28 +38,36 @@ class RandomAgent(object):
         return self.action_space.sample()
 
 
-def gather(cfg):
+def gather_agent(cfg, agent_id):
     args_env_id = 'Pong-v0'
     env = gym.make(args_env_id)
 
-    out_dir = '../data_gathered/random-agent-results/'
     agent = RandomAgent(env.action_space)
 
-    episode_count = 100
+    episode_count = 10
     reward = 0
     done = False
 
     for i in range(episode_count):
         ob = env.reset()
+        renderer = GymRenderer(env, title=f'{args_env_id}_ep{agent_id * episode_count + i}')
         step = 0
         while True:
             action = agent.act(ob, reward, done)
             ob, reward, done, _ = env.step(action)
-            plt.imshow(env.render('rgb_array'))
-            plt.savefig(f"{out_dir}{args_env_id}_ep{i}_st{step}.png")
+            renderer.render()
+            # plt.imshow(env.render('rgb_array'))
+            # plt.savefig(f"{out_dir}{args_env_id}_ep{agent_id*episode_count+i}_st{step}.png")
             step += 1
             if done:
                 break
-        rtpt.step(subtitle=f"step={i}/{episode_count}")
+        renderer.close_recorder()
+        rtpt.step(subtitle=f"ag{agent_id} step={i}/{episode_count}")
 
     env.close()
+
+
+def gather(cfg):
+    for i in range(NUM_PROCESSES):
+        proc = mp.Process(target=gather_agent, args=(cfg, env))
+        proc.start()
