eb6de78fcabf6f73849c7daa23ca44c255454bde
ThomasRot <thomasalexander.rothenbaecher@stud.tu-darmstadt.de>
2021-06-14 07:47:39 +0200

    MT-1: Path for data in pong corrected

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from engine.eval import eval
 from engine.show import show
 from engine.data_gatherer import gather
 from engine.video_splitter import split_videos
+from engine.flow_example import test_flow
 
 
 if __name__ == '__main__':
@@ -13,7 +14,8 @@ if __name__ == '__main__':
         'eval': eval,
         'show': show,
         'split_videos': split_videos,
-        'gather': gather
+        'gather': gather,
+        'test_flow': test_flow
     }
     cfg, task = get_config()
     assert task in task_dict
