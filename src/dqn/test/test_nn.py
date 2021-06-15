6363df4bf3215906e53a2bd7e2447fdbbb62865b
Dwarak Adugodi Vittal <dwarakvittal@MacBook-Pro-von-Dwarak.local>
2021-06-15 17:36:39 +0200

    Bigger lin layers

--- src/dqn/test/test_nn.py
+++ src/dqn/test/test_nn.py
@@ -15,9 +15,9 @@ class SPACE_Classifier(nn.Module):
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(13456, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 6)
+        self.fc1 = nn.Linear(13456, 1024)
+        self.fc2 = nn.Linear(1024, 256)
+        self.fc3 = nn.Linear(256, 6)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
