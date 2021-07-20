634457b7ef759a8f1e3640fc6e51740e9039c255
Dwarak Adugodi Vittal <dwarakvittal@MacBook-Pro-von-Dwarak.local>
2021-07-20 18:24:07 +0200

    genetic rl

--- src/xrl.py
+++ src/xrl.py
@@ -9,13 +9,16 @@ import matplotlib.pyplot as plt
 import PIL
 import cv2
 import os
+from numpy.lib.function_base import select
 import pandas as pd
 import seaborn as sn
+import copy
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions import Categorical
+from tqdm import tqdm
 
 from itertools import count
 from PIL import Image
@@ -34,18 +37,43 @@ if not os.path.exists(PATH_TO_OUTPUTS):
 
 model_name = lambda training_name : PATH_TO_OUTPUTS + training_name + "_model.pth"
 
+
 # define policy network
 class policy_net(nn.Module):
-    def __init__(self, nS, nH, nA): # nS: state space size, nH: n. of neurons in hidden layer, nA: size action space
+    def __init__(self, input, hidden, actions): 
         super(policy_net, self).__init__()
-        self.h = nn.Linear(nS, nH)
-        self.out = nn.Linear(nH, nA)
+        self.h = nn.Linear(input, hidden)
+        self.out = nn.Linear(hidden, actions)
 
     # define forward pass with one hidden layer with ReLU activation and sofmax after output layer
     def forward(self, x):
         x = F.relu(self.h(x))
         x = F.softmax(self.out(x), dim=1)
         return x
+
+
+def init_weights(m):
+    # nn.Conv2d weights are of shape [16, 1, 3, 3] i.e. # number of filters, 1, stride, stride
+    # nn.Conv2d bias is of shape [16] i.e. # number of filters
+    
+    # nn.Linear weights are of shape [32, 24336] i.e. # number of input features, number of output features
+    # nn.Linear bias is of shape [32] i.e. # number of output features
+    
+    if ((type(m) == nn.Linear) | (type(m) == nn.Conv2d)):
+        torch.nn.init.xavier_uniform(m.weight)
+        m.bias.data.fill_(0.00)
+
+
+# function to create random agents of given count
+def return_random_agents(num_agents):
+    agents = []
+    for _ in range(num_agents):
+        agent = policy_net(6, 32, 3)
+        for param in agent.parameters():
+            param.requires_grad = False
+        init_weights(agent)
+        agents.append(agent) 
+    return agents
 
 
 # function to plot live while training
@@ -63,6 +91,7 @@ def plot_screen(env, episode, step, second_img=None):
     plt.plot()
     plt.pause(0.0001)  # pause a bit so that plots are updated
 
+
 # helper function to calc linear equation
 def get_target_x(x1, x2, y1, y2, player_x):
     x = [x1, x2]
@@ -72,6 +101,7 @@ def get_target_x(x1, x2, y1, y2, player_x):
     # now calc target pos
     # y = mx + c
     return np.int16(m * player_x + c)
+
 
 # helper function to convert env info into custom list
 # raw_features contains player x, y, ball x, y, oldplayer x, y, oldball x, y, 
@@ -116,8 +146,9 @@ def preprocess_raw_features(env_info, last_raw_features=None):
     features.append(math.sqrt((ball_x - raw_features[8])**2 + (ball_y - raw_features[9])**2) / 25) 
     return raw_features, features
 
+
 # helper function to get features
-def get_features(env, action=1, last_raw_features=None):
+def do_step(env, action=1, last_raw_features=None):
     if action == 1:
         action = 2
     elif action == 2:
@@ -126,6 +157,7 @@ def get_features(env, action=1, last_raw_features=None):
     raw_features, features = preprocess_raw_features(info, last_raw_features)
     return raw_features, features, reward, done
 
+
 # function to select action by given features
 # 0: "NOOP",
 # 1: "FIRE",
@@ -133,7 +165,7 @@ def get_features(env, action=1, last_raw_features=None):
 # 3: "RIGHT",
 # 4: "LEFT",
 # 5: "DOWN",
-def select_action(n_actions, features, policy, global_step, logger):
+def select_action(features, policy):
     # calculate probabilities of taking each action
     probs = policy(torch.tensor(features).unsqueeze(0).float())
     # sample an action from that set of probs
@@ -142,15 +174,115 @@ def select_action(n_actions, features, policy, global_step, logger):
     # return action
     return action
 
+
+# function to run list of agents in given env
+def run_agents(env, agents):
+    start = time.perf_counter()
+    reward_agents = []
+    _ = env.reset()
+    _, _, done, info = env.step(1)
+    for agent in agents:
+        agent.eval()
+        raw_features, features, _, _ = do_step(env)
+        r = 0
+        for t in count():
+            action = select_action(features, agent)
+            raw_features, features, reward, done = do_step(env, action, raw_features)
+            r = r + reward
+            if(done):
+                break
+        reward_agents.append(r)
+    return reward_agents
+
+
+# returns average score of given agent when it runs n times
+def return_average_score(env, agent, runs):
+    score = 0.
+    for i in range(runs):
+        score += run_agents(env, [agent])[0]
+    avg_score = score/runs
+    return avg_score
+
+
+# gets avg score of every agent running n runs 
+def run_agents_n_times(agents, runs):
+    avg_score = []
+    env = AtariARIWrapper(gym.make(cfg.env_name))
+    for agent in tqdm(agents):
+        avg_score.append(return_average_score(env, agent, runs))
+    return avg_score
+
+
+# function to mutate given agent to child agent
+def mutate(agent):
+    child_agent = copy.deepcopy(agent)
+    mutation_power = 0.02 #hyper-parameter, set from https://arxiv.org/pdf/1712.06567.pdf
+    for param in child_agent.parameters():
+        if(len(param.shape)==4): #weights of Conv2D
+            for i0 in range(param.shape[0]):
+                for i1 in range(param.shape[1]):
+                    for i2 in range(param.shape[2]):
+                        for i3 in range(param.shape[3]):
+                            param[i0][i1][i2][i3]+= mutation_power * np.random.randn()
+        elif(len(param.shape)==2): #weights of linear layer
+            for i0 in range(param.shape[0]):
+                for i1 in range(param.shape[1]):
+                    param[i0][i1]+= mutation_power * np.random.randn()
+        elif(len(param.shape)==1): #biases of linear layer or conv layer
+            for i0 in range(param.shape[0]):
+                param[i0]+=mutation_power * np.random.randn()
+    return child_agent
+
+
+# function to add elite to childs 
+def add_elite(agents, sorted_parent_indexes, elite_index=None, only_consider_top_n=10):
+    candidate_elite_index = sorted_parent_indexes[:only_consider_top_n]
+    if(elite_index is not None):
+        candidate_elite_index = np.append(candidate_elite_index,[elite_index]) 
+    top_score = None
+    top_elite_index = None
+    env = AtariARIWrapper(gym.make(cfg.env_name))
+    for i in candidate_elite_index:
+        score = return_average_score(env, agents[i], runs=5)
+        print("Score for elite i ", i, " is ", score)
+        if(top_score is None):
+            top_score = score
+            top_elite_index = i
+        elif(score > top_score):
+            top_score = score
+            top_elite_index = i
+    print("Elite selected with index ",top_elite_index, " and score", top_score)
+    child_agent = copy.deepcopy(agents[top_elite_index])
+    return child_agent
+
+
+# function to create and return children from given parent agents
+def return_children(agents, sorted_parent_indexes, elite_index):  
+    children_agents = []
+    #first take selected parents from sorted_parent_indexes and generate N-1 children
+    for i in range(len(agents) - 1):
+        selected_agent_index = sorted_parent_indexes[np.random.randint(len(sorted_parent_indexes))]
+        children_agents.append(mutate(agents[selected_agent_index]))
+    #now add one elite
+    elite_child = add_elite(agents, sorted_parent_indexes, elite_index)
+    children_agents.append(elite_child)
+    elite_index = len(children_agents) - 1 #it is the last one
+    return children_agents, elite_index
+
+
+ # Compute softmax values for each sets of scores in x
+def softmax(x):
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+
 # save model helper function
-def save_model(training_name, policy, optimizer, episode, global_step):
+def save_model(training_name, policy, episode, global_step):
     if not os.path.exists(PATH_TO_OUTPUTS):
         os.makedirs(PATH_TO_OUTPUTS)
     model_path = model_name(training_name)
     print("Saving {}".format(model_path))
     torch.save({
             'policy_state_dict': policy.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
             'episode': episode,
             'global_step': global_step
             }, model_path)
@@ -159,174 +291,64 @@ def save_model(training_name, policy, optimizer, episode, global_step):
 # train main function
 def train():
     print('Experiment name:', cfg.exp_name)
-    print('Loading', cfg.env_name)
-    env = AtariARIWrapper(gym.make(cfg.env_name))
-    _ = env.reset()
-    _, _, done, info = env.step(1)
-
-    # instantiate the policy: #input_shape #hidden_node_size #action_size
-    policy = policy_net(6, 30, 3) #env.action_space.n
-    # create an optimizer
-    optimizer = torch.optim.Adam(policy.parameters(), lr=cfg.train.learning_rate)
-
-    i_episode = 0
-    global_step = 0
-
-    # load if available
-    model_path = model_name(cfg.exp_name)
-    if not os.path.isfile(model_path):
-        print("{} does not exist, starting fresh ... ".format(model_path))
-    else:
-        print("Loading {} ...".format(model_path))
-        # folder and file exists, so load and return
-        model_path = model_name(cfg.exp_name) 
-        checkpoint = torch.load(model_path)
-        policy.load_state_dict(checkpoint['policy_state_dict'])
-        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        i_episode = checkpoint['episode']
-        global_step = checkpoint['global_step']
-
-    video_every = cfg.video_steps
 
     LIVEPLOT = cfg.liveplot
     DEBUG = cfg.debug
     print('Liveplot:', LIVEPLOT)
     print('Debug Mode:', DEBUG)
 
-    num_episodes = cfg.train.num_episodes
-    log_steps = cfg.train.log_steps
+    generations = cfg.train.num_episodes
+    print('Generations:', generations)
 
-    # init tensorboard
-    log_path = os.getcwd() + cfg.logdir
-    logger = dqn.dqn_logger.DQN_Logger(log_path, cfg.exp_name, vfolder="/xrl/video/")
+    # disable gradients as we will not use them
+    torch.set_grad_enabled(False)
 
-    # Get number of actions from gym action space
-    n_actions = env.action_space.n
+    # initialize N number of agents
+    num_agents = 500
+    print('Number of agents:', num_agents)
+    agents = return_random_agents(num_agents)
 
-    #Getting the state space
-    print("Action Space {}".format(env.action_space))
-    print("State Space {}".format(env.observation_space))
-    print("State {}".format(info))
+    # How many top agents to consider as parents
+    top_limit = 20
+    print('Number of top agents:', top_limit)
+
+    # runs per generation
+    n_gen_runs = 3
+    print('Number of runs per generation:', n_gen_runs)
+
+    elite_index = None
 
     rtpt = RTPT(name_initials='DV', experiment_name=cfg.exp_name,
-                    max_iterations=num_episodes)
+                    max_iterations=generations)
     rtpt.start()
-    # train mode
-    if cfg.mode == "train":
-        sum_pos_reward = 0
-        sum_neg_reward = 0
-        sum_ep_steps = 0
-        while i_episode < num_episodes:
-            # init lists
-            rewards = []
-            actions = []
-            states  = []
-            # reset env
-            obs = env.reset()
-            raw_features, features, _, _ = get_features(env)
-            # timer stuff
-            start = time.perf_counter()
-            episode_start = start
-            episode_time = 0
-            # track for logging
-            episode_steps = 0
-            pos_reward_count = 0
-            neg_reward_count = 0
-            for t in count():
-                # logging stuff
-                global_step += 1
-                # log video 
-                if i_episode % cfg.video_steps == 0:
-                    # log current frame
-                    screen = env.render(mode='rgb_array')
-                    pil_img = Image.fromarray(screen).resize((128, 128), PIL.Image.BILINEAR)
-                    # convert image to opencv
-                    opencv_img = np.asarray(pil_img).copy()
-                    # fill video buffer
-                    logger.fill_video_buffer(opencv_img)
-                # timer stuff
-                end = time.perf_counter()
-                episode_time = end - episode_start
-                start = end
-                # select action
-                action = select_action(n_actions, features, policy, global_step, logger)
-                #Taking the action and getting the reward and outcome raw_features
-                raw_features, features, reward, done = get_features(env, action, raw_features)
-                #buffer features actions and reward
-                states.append(features)
-                actions.append(action)
-                rewards.append(reward)
-                # for logging
-                if reward > 0:
-                    pos_reward_count += 1
-                if reward < 0:
-                    neg_reward_count += 1
-                # timer stuff
-                end = time.perf_counter()
-                step_time = end - start
-                if LIVEPLOT:
-                    plot_screen(env, i_episode+1, t+1)
-                if done:
-                    print(
-                        'exp: {}, episode: {}, step: {}, +reward: {:.2f}, -reward: {:.2f}, s-time: {:.4f}s, e-time: {:.4f}s        '.format(
-                            cfg.exp_name, i_episode + 1, t + 1, pos_reward_count, neg_reward_count, 
-                            step_time, episode_time), end="\r")
-                    episode_steps = t + 1
-                    break
-            # optimize after done
-            #TODO: which algo?
-            # log episode
-            sum_pos_reward += pos_reward_count
-            sum_neg_reward += neg_reward_count
-            sum_ep_steps += episode_steps
-            if (i_episode + 1) % 20 == 0:
-                logger.log_episode(sum_ep_steps / 20, sum_pos_reward / 20, sum_neg_reward / 20, i_episode, global_step)
-                sum_pos_reward, sum_neg_reward, sum_ep_steps = 0, 0, 0
-            if i_episode % cfg.video_steps == 0:
-                logger.save_video(cfg.exp_name)
-            # iterate to next episode
-            i_episode += 1
-            # checkpoint saver
-            if i_episode % cfg.train.save_every == 0:
-                save_model(cfg.exp_name, policy, optimizer, i_episode, global_step)
-            rtpt.step()
-    # now eval
-    # TODO: still buggy
-    if cfg.mode == "eval":
-        print("Evaluating...")
-        # Initialize the environment and state
-        obs = env.reset()
-        raw_features, features, _, _ = get_features(env)
-        pos_reward_count = 0
-        neg_reward_count = 0
-        for t in count():
-            # log current frame
-            screen = env.render(mode='rgb_array')
-            pil_img = Image.fromarray(screen).resize((128, 128), PIL.Image.BILINEAR)
-            # convert image to opencv
-            opencv_img = np.asarray(pil_img).copy()
-            # fill video buffer
-            if i_episode % video_every == 0:
-                logger.fill_video_buffer(opencv_img)
-            # select action
-            action = select_action(n_actions, features, policy, global_step, logger)
-            #Taking the action and getting the reward and outcome state
-            raw_features, features, reward, done = get_features(env, action, raw_features)
-            # for logging
-            if reward > 0:
-                pos_reward_count += 1
-            if reward < 0:
-                neg_reward_count += 1
-            if LIVEPLOT:
-                plot_screen(env, i_episode+1, t+1)
-            if done:
-                print(
-                    'exp: {}, step: {}, +reward: {:.2f}, -reward: {:.2f}        '.format(
-                        cfg.exp_name, t + 1, pos_reward_count, neg_reward_count))
-                episode_steps = t + 1
-                break
-        logger.save_video(cfg.exp_name)
+    for generation in range(generations):
+        print("Starting generation", generation)
+        # return rewards of agents
+        rewards = run_agents_n_times(agents, n_gen_runs) #return average of 3 runs
+ 
+        # sort by rewards
+        # reverses and gives top values (argsort sorts by ascending by default) https://stackoverflow.com/questions/16486252/is-it-possible-to-use-argsort-in-descending-order
+        sorted_parent_indexes = np.argsort(rewards)[::-1][:top_limit] 
+        print("")
+        print("")
+        
+        top_rewards = []
+        for best_parent in sorted_parent_indexes:
+            top_rewards.append(rewards[best_parent])
+       
+        print("Generation ", generation, " | Mean rewards: ", np.mean(rewards), " | Mean of top 5: ",np.mean(top_rewards[:5]))
+        #print(rewards)
+        print("Top ",top_limit," scores", sorted_parent_indexes)
+        print("Rewards for top: ",top_rewards)
+        
+        # setup an empty list for containing children agents
+        children_agents, elite_index = return_children(agents, sorted_parent_indexes, elite_index)
+ 
+        # kill all agents, and replace them with their children
+        agents = children_agents
+        rtpt.step()
 
 
 if __name__ == '__main__':
-    train()
+    if cfg.mode == "train":
+        train()
