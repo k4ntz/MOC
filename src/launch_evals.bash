97f50c4413e8de5b84acc37ba5cfd785b77325a5
Quentin Delfosse <q.delfosse@gmail.com>
2022-09-13 23:31:51 +0200

    Fixed script all arch type

--- src/launch_evals.bash
+++ src/launch_evals.bash
@@ -4,19 +4,22 @@ for game in air_raid boxing carnival pong mspacam spaceinvaders riverraid
 do
   for seed in 0 1 2 3 4
   do
+    for at in baseline +m +moc
+    do
     if nvidia-smi | grep "Processes" > /dev/null ; then
-      # No running processes found
-      if (($dev < 8)); then
-        echo "CUDA_VISIBLE_DEVICES=$dev python3 main.py --task eval --config configs/atari_$game.yaml --arch-type baseline seed $seed"
-        CUDA_VISIBLE_DEVICES=$dev nohup python3 main.py --task eval --config configs/atari_$game.yaml --arch-type baseline seed $seed > nohup$i.out 2>&1 &
-        dev=$((dev+1))
-        i=$((i+1))
+        # No running processes found
+        if (($dev < 8)); then
+          echo "CUDA_VISIBLE_DEVICES=$dev python3 main.py --task eval --config configs/atari_$game.yaml --arch-type baseline seed $seed"
+          CUDA_VISIBLE_DEVICES=$dev nohup python3 main.py --task eval --config configs/atari_$game.yaml --arch-type $at seed $seed > nohup$i.out 2>&1 &
+          dev=$((dev+1))
+          i=$((i+1))
+        else
+          dev=0
+          sleep 20
+        fi
       else
-        dev=0
-        sleep 20
+        sleep 60
       fi
-    else
-      sleep 60
-    fi
+    done
   done
 done
