a997b438e07a227cbe1ebcf243278e1490642c68
Quentin Delfosse <q.delfosse@gmail.com>
2022-09-13 23:38:42 +0200

    Fixed script all arch type

--- src/launch_evals.bash
+++ src/launch_evals.bash
@@ -9,8 +9,8 @@ do
     if nvidia-smi | grep "Processes" > /dev/null ; then
         # No running processes found
         if (($dev < 8)); then
-          echo "CUDA_VISIBLE_DEVICES=$dev python3 main.py --task eval --config configs/atari_$game.yaml --arch-type baseline seed $seed"
-          CUDA_VISIBLE_DEVICES=$dev nohup python3 main.py --task eval --config configs/atari_$game.yaml --arch-type $at seed $seed > nohup$i.out 2>&1 &
+          echo "CUDA_VISIBLE_DEVICES=$dev python3 main.py --task eval --config configs/atari_$game.yaml --arch-type $at seed $seed"
+          # CUDA_VISIBLE_DEVICES=$dev nohup python3 main.py --task eval --config configs/atari_$game.yaml --arch-type $at seed $seed > nohup$i.out 2>&1 &
           dev=$((dev+1))
           i=$((i+1))
         else
