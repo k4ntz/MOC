aaa5d489fa41dd1774f55f71d490336e231783d6
Quentin Delfosse <q.delfosse@gmail.com>
2022-09-13 23:24:15 +0200

    Fixed device_id

--- src/launch_evals.bash
+++ src/launch_evals.bash
@@ -7,8 +7,8 @@ do
     if nvidia-smi | grep "Processes" > /dev/null ; then
       # No running processes found
       if (($dev < 8)); then
-        echo "CUDA_VISIBLE_DEVICES=$dev python3 create_dataset.py -f $folder -g $game"
-        CUDA_VISIBLE_DEVICES=$dev nohup python3 main.py --task eval --config configs/atari_$game.yaml --arch-type baseline seed 1 > nohup$i.out 2>&1 &
+        echo "CUDA_VISIBLE_DEVICES=$dev python3 main.py --task eval --config configs/atari_$game.yaml --arch-type baseline seed $seed"
+        CUDA_VISIBLE_DEVICES=$dev nohup python3 main.py --task eval --config configs/atari_$game.yaml --arch-type baseline seed $seed > nohup$i.out 2>&1 &
         dev=$((dev+1))
         i=$((i+1))
       else
